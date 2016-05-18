1fc9c258c2d59abc787ea89b5710888f3b119228
RomanKol <kollatschny.roman@web.de>
2016-05-18 18:04:06 +0200

    Added route for smartmirror user view

--- app/routes/smartmirror.js
+++ app/routes/smartmirror.js
@@ -5,8 +5,12 @@ var router = express.Router();
 
 
 /* GET smartmirror interface page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'SmartMirror Interface' });
+});
+
+router.get('/:id', (req, res, next) => {
+  res.render('index', {title:'Smartmirror user: ' + req.params.id});
 });
 
 module.exports = router;
--- test/routes/smartmirror.js
+++ test/routes/smartmirror.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var chai = require('chai');
 var expect = require('chai').expect;
 var chaiHttp = require('chai-http');
@@ -6,11 +8,24 @@ chai.use(chaiHttp);
 
 describe("Webview Smartmirror Check", function() {
 
-  it("Smartmirror Page can be viewed", function(done) {
+  it("displays smartmirror page", function(done) {
     chai.request('http://localhost:3000/smartmirror')
     .get('/')
     .end((err, res) => {
+      expect(err).to.be.null;
       expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  it('displays users specific webview', function (done) {
+    let userid = '1234567890-abcdefghijklnopqrstuvwxyz';
+    chai.request('http://localhost:3000/smartmirror/' + userid)
+    .get('/')
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200)
+      .and.to.be.html;
       done();
     });
   });
