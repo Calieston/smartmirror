747c834b5540980f3e886d3f8a9613bb6cff24ef
Calieston <calieston@hotmail.com>
2016-08-28 22:15:42 +0200

    Added config_example.js, ignore max line rule

---
+++ app/config_example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Link to modul server
+  moduleServer: '***',
+  // Name for audio file used for voice control
+  fileName: '***',
+  // Language for speechtotext (examples: en-us, nl-be, fr-fr, …)
+  lang: '***',
+  // Google speech api key
+  key: '***',
+  // Recording duration (max = 15000)
+  duration: 10000,
+  // Gesture types used by leap motion controller (swipe left, swipe right, ...)
+  gestureTypes: ['***', '***'],
+};
